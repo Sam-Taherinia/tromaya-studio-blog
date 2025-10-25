02bff1075d1e9abe3cfed463de170ad263e9a7c8
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-25 18:54:59 +0330

    updated security config, added error handlers, created class that BlogUserDetails implements UserDetails

--- backend/src/main/java/com/tromayastudio/blog/repositories/UserRepository.java
+++ backend/src/main/java/com/tromayastudio/blog/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.tromayastudio.blog.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> { // we used JpaRepository because it gives us all that CRUD behavior and some pagination as well
+
+    Optional<User> findByEmail(String email);
 
 }
