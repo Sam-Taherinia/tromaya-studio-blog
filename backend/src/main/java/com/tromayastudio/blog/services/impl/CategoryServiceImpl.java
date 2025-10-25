8e6b69cb99f006e278518faaeaf1ea0470ddcbdd
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-25 17:45:06 +0330

    updated category controller, service, dto, mapper and repository. added delete category functionality

--- backend/src/main/java/com/tromayastudio/blog/services/impl/CategoryServiceImpl.java
+++ backend/src/main/java/com/tromayastudio/blog/services/impl/CategoryServiceImpl.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service // this will make the class available for auto-wiring and also inject any dependencies that we declare in this class constructor
 @RequiredArgsConstructor
@@ -27,5 +29,16 @@ public class CategoryServiceImpl implements CategoryService {
             throw new IllegalArgumentException("Category already exists: " +  category.getName());
         }
         return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(UUID id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent()) {
+            if (!category.get().getPosts().isEmpty()) {
+                throw new IllegalStateException("Category already has posts: " +  category.get().getPosts().size());
+            }
+            categoryRepository.deleteById(id);
+        }
     }
 }
