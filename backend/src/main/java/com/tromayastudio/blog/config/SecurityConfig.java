0b0b69db4e43d39cdf4d5560c67102abd1111f64
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-29 17:25:51 +0330

    implemented user authentication and security

--- backend/src/main/java/com/tromayastudio/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/tromayastudio/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.tromayastudio.blog.config;
 
+import com.tromayastudio.blog.domain.entities.User;
+import com.tromayastudio.blog.repositories.UserRepository;
+import com.tromayastudio.blog.security.BlogUserDetailsService;
+import com.tromayastudio.blog.security.JwtAuthenticationFilter;
+import com.tromayastudio.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,17 +12,43 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+            AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepository);
+        // create a dummy user (not production system standard)
+        String email = "tromayastudio@test.com";
+        userRepository.findByEmail(email).orElseGet(()-> { // it creates the user if only it already doesn't exist
+            User newUser = User.builder()
+                    .name("admin")
+                    .email(email)
+                    .password(passwordEncoder().encode("123456"))
+                    .build();
+            return userRepository.save(newUser);
+        });
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -26,7 +57,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
