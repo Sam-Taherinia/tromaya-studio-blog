4c23d327639ff64ac6f972a93f69c9935cde7f52
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-19 18:33:05 +0330

    adds delete tag endpoint

--- backend/src/main/java/com/tromayastudio/blog/controllers/TagController.java
+++ backend/src/main/java/com/tromayastudio/blog/controllers/TagController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tags")
@@ -36,5 +37,11 @@ public class TagController {
                 createdTagRespons,
                 HttpStatus.CREATED
         );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/com/tromayastudio/blog/services/TagService.java
+++ backend/src/main/java/com/tromayastudio/blog/services/TagService.java
@@ -4,9 +4,12 @@ import com.tromayastudio.blog.domain.entities.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getAllTags();
     List<Tag> createTags(Set<String> tagNames);
+
+    void deleteTag(UUID id);
 
 }
