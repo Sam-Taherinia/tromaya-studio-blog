3e6134f56c40dcf436886f0191d3fe21bcb886c3
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-19 14:33:42 +0330

    adds create tag endpoint

--- backend/src/main/java/com/tromayastudio/blog/controllers/TagController.java
+++ backend/src/main/java/com/tromayastudio/blog/controllers/TagController.java
@@ -1,14 +1,14 @@
 package com.tromayastudio.blog.controllers;
 
+import com.tromayastudio.blog.domain.dtos.CreateTagsRequest;
 import com.tromayastudio.blog.domain.dtos.TagResponse;
 import com.tromayastudio.blog.domain.entities.Tag;
 import com.tromayastudio.blog.mappers.TagMapper;
 import com.tromayastudio.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,16 @@ public class TagController {
         List<Tag> tags = tagService.getAllTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagRespons = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagRespons,
+                HttpStatus.CREATED
+        );
     }
 }
--- backend/src/main/java/com/tromayastudio/blog/services/TagService.java
+++ backend/src/main/java/com/tromayastudio/blog/services/TagService.java
@@ -3,7 +3,10 @@ package com.tromayastudio.blog.services;
 import com.tromayastudio.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getAllTags();
+    List<Tag> createTags(Set<String> tagNames);
+
 }
